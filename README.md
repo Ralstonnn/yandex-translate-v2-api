7c2746ad7b4be4824ab0a9ffe899a19190df6061
Ralstonnn <zigthehedge2002@gmail.com>
2022-08-27 02:31:50 +0500

    moved to ts

--- README.md
+++ README.md
@@ -6,31 +6,56 @@ You have to ways to use this module
 ```targetLanguage``` is English by default. You can find the list of available languages and their codes here https://cloud.yandex.com/en-ru/docs/translate/concepts/supported-languages
 
 ## Usage example
-```
+```ts
 import { Translator } from "yandex-translate-v2-api";
 
 const t = Translator.Api(API_KEY, FOLDER_ID, "ja"); // Language is optional. English by default
-await t.createTranslatedFile(obj, path, "js");
+await t.createTranslatedFile(obj, path, "js"); // Type is optional "js" by default
 ```
 
 ## Methods
 ### async translate
-```translate(texts: string[])``` The methods takes string array and returns string array with translated values
+```ts
+async translate(texts: string[] | string): Promise<string[]>
+``` 
+The methods takes string array and returns string array with translated values
 
 ### async detect 
-```detect(text: string)``` The method takes string and returns language it's written in
+```ts
+async detect(text: string): Promise<string>
+``` 
+The method takes string and returns language it's written in
 
-### async getSrtingArrayFromObject
-```getSrtingArrayFromObject(obj: {})``` The method takes JSON formatted object with strings to translate as values and returns all strings as 1 dimensional array 
+### getStringArrayFromObject
+```ts
+getStringArrayFromObject(
+  obj: { [key: string]: string | object }
+): string[]
+``` 
+The method takes JSON formatted object with strings to translate as values and returns all strings as 1 dimensional array 
 
 ### async translateStabilized
-```translateStabilized(texts: string[])``` The same method as ```translate``` but for arrays with the total strings length more than 10_000 characters
+```ts
+async translateStabilized(texts: string[] | string): Promise<string[]>
+``` 
+The same method as ```translate``` but for arrays with the total strings length more than 10_000 characters
 
 ### async getTranslatedObject
-```getTranslatedObject(obj: {})``` The method takes JSON formatted object with strings to translate as values and returns object with translated values
+```ts
+async getTranslatedObject(
+  obj: { [key: string]: string | object }
+): Promise<{ [key: string]: string | object }>
+``` 
+The method takes JSON formatted object with strings to translate as values and returns object with translated values
 
 ### async createTranslatedFile
-```createTranslatedFile(obj: {}, filePath: string, type: "js" | "json")``` 
+```ts
+async createTranslatedFile(
+  obj: { [key: string]: string | object }, 
+  filePath: string, 
+  type: "js" | "json"
+): Promise<void>
+``` 
 * As a first argument method takes JSON formatted object with strings to translate as values. 
 * Second argument is a string with a path to save translated file to. 
 * (Optional) Third argument is the type of exported object. It can be exported as a plain JSON or with ```export default``` as a JS object
--- index.js
+++ index.js
@@ -1,162 +1,135 @@
 import fetch from "node-fetch";
-import fs from "fs";
-
+import * as fs from "fs";
 export class Translator {
-  targetLanguage;
-  #API_KEY = null;
-  #IAM_TOKEN = null;
-  #FOLDER_ID;
-  #TRANSLATE_URL =
-    "https://translate.api.cloud.yandex.net/translate/v2/translate";
-  #DETECT_URL = "https://translate.api.cloud.yandex.net/translate/v2/detect";
-  #translatedArray = null;
-
-  // Static factory to use with API_KEY
-  static Api(apiKey, folderId, targetLanguage = "en") {
-    const translator = new Translator();
-    translator.#API_KEY = apiKey;
-    translator.#FOLDER_ID = folderId;
-    translator.targetLanguage = targetLanguage;
-    return translator;
-  }
-
-  // Static factory to use with IAM_TOKEN
-  static Imt(iamToken, folderId, targetLanguage = "en") {
-    const translator = new Translator();
-    translator.#IAM_TOKEN = iamToken;
-    translator.#FOLDER_ID = folderId;
-    translator.targetLanguage = targetLanguage;
-    return translator;
-  }
-
-  async translate(texts) {
-    if (typeof texts === "string") texts = [texts];
-
-    const request = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: this.#API_KEY
-          ? `Api-Key ${this.#API_KEY}`
-          : `Bearer ${this.#IAM_TOKEN}`,
-      },
-      body: JSON.stringify({
-        targetLanguageCode: this.targetLanguage,
-        texts: texts,
-        folderId: this.#FOLDER_ID,
-      }),
-    };
-
-    let respone = await fetch(this.#TRANSLATE_URL, request);
-    respone = await respone.text();
-    let result = JSON.parse(respone).translations;
-    result = result.map((item) => item.text);
-
-    return result;
-  }
-
-  async detect(text) {
-    const request = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: this.#API_KEY
-          ? `Api-Key ${this.#API_KEY}`
-          : `Bearer ${this.#IAM_TOKEN}`,
-      },
-      body: JSON.stringify({
-        text: text,
-        folderId: this.#FOLDER_ID,
-      }),
-    };
-
-    let response = await fetch(this.#DETECT_URL, request);
-    response = await response.text();
-    response = JSON.parse(response).languageCode;
-
-    return response;
-  }
-
-  async getSrtingArrayFromObject(obj) {
-    let result = [];
-
-    for (const [key, value] of Object.entries(obj)) {
-      if (typeof value === "string") {
-        result.push(value);
-      } else if (typeof value === "object") {
-        const res = await this.getSrtingArrayFromObject(value);
-        result.push(...res);
-      }
+    targetLanguage = "";
+    #API_KEY = "";
+    #IAM_TOKEN = "";
+    #FOLDER_ID = "";
+    #TRANSLATE_URL = "https://translate.api.cloud.yandex.net/translate/v2/translate";
+    #DETECT_URL = "https://translate.api.cloud.yandex.net/translate/v2/detect";
+    #translatedArray = [];
+    static Api(apiKey, folderId, targetLanguage = "en") {
+        const translator = new Translator();
+        translator.#API_KEY = apiKey;
+        translator.#FOLDER_ID = folderId;
+        translator.targetLanguage = targetLanguage;
+        return translator;
     }
-
-    return result;
-  }
-
-  async translateStabilized(texts) {
-    if (typeof texts === "string") texts = [texts];
-
-    let result = [];
-    if (texts.join("").length > 10000) {
-      let silcedArray = await this.translateStabilized(
-        texts.slice(0, texts.length - 5)
-      );
-      result.push(...silcedArray);
-      silcedArray = await this.translateStabilized(
-        texts.slice(texts.length - 5, texts.length)
-      );
-      result.push(...silcedArray);
-    } else {
-      let tempRes = await this.translate(texts, this.targetLanguage);
-      result.push(...tempRes);
+    static Imt(iamToken, folderId, targetLanguage = "en") {
+        const translator = new Translator();
+        translator.#IAM_TOKEN = iamToken;
+        translator.#FOLDER_ID = folderId;
+        translator.targetLanguage = targetLanguage;
+        return translator;
     }
-
-    this.#translatedArray = result;
-    return result;
-  }
-
-  async getTranslatedObject(obj) {
-    if (!this.#translatedArray) {
-      let tmp = await this.getSrtingArrayFromObject(obj);
-      tmp = await this.translateStabilized(tmp);
+    async translate(texts) {
+        if (typeof texts === "string")
+            texts = [texts];
+        const request = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: this.#API_KEY
+                    ? `Api-Key ${this.#API_KEY}`
+                    : `Bearer ${this.#IAM_TOKEN}`,
+            },
+            body: JSON.stringify({
+                targetLanguageCode: this.targetLanguage,
+                texts: texts,
+                folderId: this.#FOLDER_ID,
+            }),
+        };
+        let response = (await fetch(this.#TRANSLATE_URL, request));
+        response = await response.text();
+        response = JSON.parse(response).translations;
+        return response.map((item) => item.text);
+        ;
     }
-
-    let result = {};
-
-    for (const [key, value] of Object.entries(obj)) {
-      if (typeof value === "string") {
-        result[key] = this.#translatedArray.shift();
-      } else if (typeof value === "object") {
-        const res = await this.getTranslatedObject(obj[key]);
-        result[key] = { ...res };
-      }
+    async detect(text) {
+        const request = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: this.#API_KEY
+                    ? `Api-Key ${this.#API_KEY}`
+                    : `Bearer ${this.#IAM_TOKEN}`,
+            },
+            body: JSON.stringify({
+                text: text,
+                folderId: this.#FOLDER_ID,
+            }),
+        };
+        let response = (await fetch(this.#DETECT_URL, request));
+        response = await response.text();
+        response = JSON.parse(response).languageCode;
+        return response;
     }
-
-    return result;
-  }
-
-  async createTranslatedFile(obj, filePath, type = "js") {
-    let translatedObj = await this.getTranslatedObject(obj);
-
-    if (type.toLowerCase() === "json") {
-      fs.writeFile(filePath, JSON.stringify(translatedObj), (err) => {
-        if (err) {
-          console.error(err);
-          return;
+    getStringArrayFromObject(obj) {
+        let result = [];
+        Object.values(obj).forEach((value) => {
+            if (typeof value === "string") {
+                result.push(value);
+            }
+            else if (typeof value === "object") {
+                const res = this.getStringArrayFromObject(value);
+                result.push(...res);
+            }
+        });
+        return result;
+    }
+    async translateStabilized(texts) {
+        if (typeof texts === "string")
+            texts = [texts];
+        let result = [];
+        if (texts.join("").length > 10000) {
+            let silcedArray = await this.translateStabilized(texts.slice(0, texts.length - 5));
+            result.push(...silcedArray);
+            silcedArray = await this.translateStabilized(texts.slice(texts.length - 5, texts.length));
+            result.push(...silcedArray);
         }
-        console.log("File was successfuly created!");
-      });
-    } else if (type.toLowerCase() === "js") {
-      fs.writeFile(
-        filePath,
-        `export default ${JSON.stringify(translatedObj)}`,
-        (err) => {
-          if (err) {
-            console.error(err);
-            return;
-          }
-          console.log("File was successfuly created!");
+        else {
+            let tempRes = await this.translate(texts);
+            result.push(...tempRes);
         }
-      );
+        this.#translatedArray = result;
+        return result;
     }
-  }
+    async getTranslatedObject(obj) {
+        if (this.#translatedArray.length === 0) {
+            let tmp = this.getStringArrayFromObject(obj);
+            await this.translateStabilized(tmp);
+        }
+        let result = {};
+        for (const [key, value] of Object.entries(obj)) {
+            if (typeof value === "string") {
+                result[key] = this.#translatedArray.shift();
+            }
+            else if (typeof value === "object") {
+                const res = await this.getTranslatedObject(value);
+                result[key] = { ...res };
+            }
+        }
+        return result;
+    }
+    async createTranslatedFile(obj, filePath, type = "js") {
+        let translatedObj = await this.getTranslatedObject(obj);
+        if (type.toLowerCase() === "json") {
+            fs.writeFile(filePath, JSON.stringify(translatedObj), (err) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                console.log("File was successfuly created!");
+            });
+        }
+        else if (type.toLowerCase() === "js") {
+            fs.writeFile(filePath, `export default ${JSON.stringify(translatedObj)}`, (err) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                console.log("File was successfuly created!");
+            });
+        }
+    }
 }
